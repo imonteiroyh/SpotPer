f8180535858c87cc9fdf6f6d6b3e159c4edb8ba8
imonteiroyh <imonteiroyh@gmail.com>
2023-11-05 09:17:59 -0300

    Creating the main functionalities to handle the playlists

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,113 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from services import database_queries
 
 routing = Blueprint('routing', __name__)
 
-@routing.route('/')
+@routing.route('/', methods=['GET'])
 def home():
-    return 'Página Inicial'
+    # PRIMEIRO CARD DEVE SER PRA REDIRECIONAR PRA CRIAÇÃO DE PLAYLIST
+    # QUALQUER CARD CLICADO DEVE REDIRECIONAR PARA AS INFORMAÇÕES DAS FAIXAS NA PLAYLIST
+
+    if request.method == 'GET':
+        response = database_queries.get_playlists()
+        return response
+
+@routing.route('/playlist/<int:playlist_code>', methods=['GET', 'DELETE'])
+def playlist(playlist_code):
+    
+    if request.method == 'GET':
+        response = database_queries.get_playlist(playlist_code)
+        result = response['result']
+
+        return result if result else {
+            'error': 'Playlist not found'
+        }
+    
+    if request.method == 'DELETE':
+        response = database_queries.delete_playlist(playlist_code)
+        message = response['message']
+
+        if message == 'success': 
+            return {
+                'message': 'Playlist deleted'
+            }
+
+        elif message == 'no changes':
+            return {
+                'message': 'No changes'
+            }
+    
+        return {
+            'error': 'Error deleting playlist'
+        }
+    
+@routing.route('/playlist/<int:playlist_code>/removeTrack', methods=['DELETE'])
+def remove_track(playlist_code):
+    # album_code = request.args.get('album_code')
+    # album_media_number = request.args.get('album_media_number')
+    # track_number = request.args.get('track_number')
+
+    if request.method == 'DELETE':
+        album_code = 1
+        album_media_number = 1
+        track_number = 1
+
+        response = database_queries.remove_track(playlist_code, album_code, album_media_number, track_number)
+
+        return response
+    
+
+@routing.route('/playlist/<int:playlist_code>/addTrack', methods=['GET', 'POST'])
+def add_track(playlist_code):
+    # album_code = request.args.get('album_code')
+    # album_media_number = request.args.get('album_media_number')
+    # track_number = request.args.get('track_number')
+
+    if request.method == 'GET':
+        # mostrar como se fosse no '/'
+        # aqui se quiser mostrar quais tracks já estão na playlist
+        response = database_queries.get_playlist(playlist_code)
+        result = response['result']
+
+
+        return result if result else {
+            'error': 'Playlist not found'
+        }
+
+    if request.method == 'POST':
+        # aí aqui o mesmo esquema do '/' de receber os dados só que agora sem playlist_name
+        # list of tuples (album_code, album_media_number, track_number)
+        tracks = [
+        (1, 1, 1),
+        (1, 1, 2)
+        ]
+
+        response = database_queries.populate_playlist(playlist_code, tracks)
+
+        return response
+
+    
+@routing.route('/createPlaylist', methods=['GET', 'POST'])
+def create_playlist():
+    if request.method == 'GET':
+        response = database_queries.get_albums()
+        result = response['result']
+
+        return result if result else {
+            'error': 'Error getting albums'
+        }
+    
+    if request.method == 'POST':
+        # string
+        playlist_name = 'x' 
+
+        # list of tuples (album_code, album_media_number, track_number)
+        tracks = [
+        (1, 1, 2),
+        (2, 1, 1),
+        (3, 1, 1)
+        ]
+
+        response = database_queries.create_playlist(playlist_name, tracks)
+
+        return response
--- services/database_queries.py
+++ services/database_queries.py
@@ -1,20 +1,165 @@
-from database.connection import connect_to_database
-from database.database import execute_query
+from database.query import Query
 
-connection, cursor = connect_to_database()
-
-def get_tracks():
+def get_playlists():
     query = '''
         SELECT
             *
         FROM
-            track
+            playlist
     '''
 
-    result = execute_query(cursor, query)
+    response = Query(query).response
     
-    response = {
-        'result': result
-    }
+    return response 
+
+
+def get_available_playlist_code():
+    query = '''
+        DECLARE @NextCode INT
+
+        SELECT
+            @NextCode = MAX(code) + 1
+        FROM
+            playlist
+
+        IF @NextCode IS NULL
+            SET @NextCode = 1
+
+        SELECT @NextCode AS next_code
+    '''
+
+    response = Query(query, get=True).response
+    
+    code = response['result'][0]['next_code']
+    
+    return code
+
+
+def get_playlist(playlist_code):
+    query = '''
+        SELECT
+            p.name AS playlist_name,
+            p.creation_date,
+            p.total_execution_time,
+            a.description AS album_description,
+            t.description AS track_description,
+            t.execution_time AS track_execution_time
+        FROM
+            playlist p
+        LEFT JOIN
+            track_playlist tp
+        ON
+            p.code = tp.playlist_code
+        LEFT JOIN
+            track t
+        ON
+            tp.album_code = t.album_code
+            AND tp.album_media_number = t.album_media_number
+            AND tp.track_number = t.number
+        LEFT JOIN
+            album a
+        ON
+            t.album_code = a.code
+            AND t.album_media_number = a.media_number
+        WHERE
+            p.code = ?
+    '''
+
+    response = Query(query, playlist_code).response
+    
+    return response
+
+
+def get_albums():
+    query = '''
+        SELECT
+            a.code AS album_code,
+            a.description AS album_description,
+            t.description AS track_description,
+            t.execution_time AS track_execution_time
+        FROM
+            album a
+        LEFT JOIN
+            track t
+        ON
+            a.code = t.album_code
+            AND a.media_number = t.album_media_number
+    '''
+
+    response = Query(query).response
+    
+    return response
+
+
+def populate_playlist(playlist_code, tracks, create=False):
+    query = '''
+        INSERT INTO track_playlist(
+            album_code, album_media_number, track_number, playlist_code, last_played, times_played
+        )
+        VALUES
+            (?, ?, ?, ?, NULL, 0)
+    '''
+    
+    for track in tracks:
+        album_code, album_media_number, track_number = track
+        response = Query(query, album_code, album_media_number, track_number, playlist_code).response
+
+        if response['message'] == 'no changes':
+            return {
+                'error': f'Error inserting track {track} in the playlist'
+            }
+    
+    if create:
+        return {
+            'message': 'Playlist created'
+        }
+    else:
+        return {
+            'message': 'Tracks inserted into the playlist'
+        }
+
+
+def create_playlist(playlist_name, tracks):
+    query = '''
+        INSERT INTO playlist(code, name, creation_date) 
+        VALUES
+            (?, ?, CONVERT(VARCHAR, GETDATE(), 105))
+    '''
+
+    playlist_code = get_available_playlist_code()
+    create_playlist_response = Query(query, playlist_code, playlist_name).response
+
+    if create_playlist_response['message'] == 'no changes':
+        return {
+            'error': 'Error creating playlist'
+        }
+    
+    populate_playlist_response = populate_playlist(playlist_code, tracks, create=True)
+
+    return populate_playlist_response
+
+
+def delete_playlist(playlist_code):
+    query = '''
+        DELETE FROM
+            playlist
+        WHERE
+            playlist.code = ?
+    '''
+
+    response = Query(query, playlist_code).response
+
+    return response
+
+
+def remove_track(playlist_code, album_code, album_media_number, track_number):
+    query = '''
+        DELETE FROM
+            track_playlist
+        WHERE
+            playlist_code = ? AND album_code = ? AND album_media_number = ? AND track_number = ?
+    '''
+
+    response = Query(query, playlist_code, album_code, album_media_number, track_number).response
 
     return response
