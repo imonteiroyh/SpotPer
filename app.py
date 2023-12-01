e96e6c040e2b4af236ab0b90148f95ac8e65c4be
imonteiroyh <imonteiroyh@gmail.com>
2023-12-01 09:27:10 -0300

    Fix: Corrigindo caminhos para arquivos estáticos

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from config.app import HOST, PORT, TEMPLATE_FOLDER
-from app.routes import routing
 from flask import Flask
-import os
+from config.app import HOST, PORT, TEMPLATE_FOLDER, STATIC_FOLDER
+from app.routes import routing
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder=TEMPLATE_FOLDER,
+    static_folder=STATIC_FOLDER
+)
+
 app.register_blueprint(routing)
-
-app.config['template_folder'] = TEMPLATE_FOLDER
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
--- config/app.py
+++ config/app.py
@@ -1,3 +1,4 @@
 HOST = 'localhost'
 PORT = 8080
 TEMPLATE_FOLDER = 'app/views'
+STATIC_FOLDER = 'app/static'
